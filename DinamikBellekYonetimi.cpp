b809402f3441939d6f48a8baa2b46382d5089b3e
Melik KARACA <61596919+mkaraca25@users.noreply.github.com>
2020-05-18 22:51:27 +0300

    Matris
    Public ve private ile matrisin şanslı elemanı ve gizemli matris projesi

---
+++ DinamikBellekYonetimi.cpp
@@ -0,0 +1,9 @@
+#include "DinamikBellekYonetimi.h"
+using namespace std;
+bool DinamikBellekYonetimi::GizemliMatrisMi(int satir,int sutun) //Yardýmcý Fonksyon tanýmý
+{
+	int **matrisPtr=new int*[satir];               //Ýki boyutlu dizinin satýr sayýsýnýn deklare edilmesi.
+	int a,b,i,j,k,d;                              //Kullanacaðýmýz deðiþkenler
+	int toplam1=0,toplam2=0,toplam3=0,toplam4=0;
+	int M=satir*(satir*satir+1)/2;               //Proje'de verilen baðýntý.
+}
